7023209b5ef17b5e4ff7aca37b9c7d19d86281f8
Kavita Raikwar <kavitavsd@gmail.com>
2022-04-14 23:09:14 +0530

    first commit

---
+++ gallary.js
@@ -0,0 +1,19 @@
+console.log('It is working fine');
+
+
+function resetEverything(){
+    document.getElementById("panel-1").classList.remove("fulsize");
+    document.getElementById("panel-2").classList.remove("fulsize");
+    document.getElementById("panel-3").classList.remove("fulsize");
+    document.getElementById("panel-4").classList.remove("fulsize");
+    document.getElementById("panel-5").classList.remove("fulsize");
+    document.getElementById("panel-6").classList.remove("fulsize");
+    document.getElementById("panel-7").classList.remove("fulsize");
+
+}
+
+function expand(a){
+     console.log(a);
+     resetEverything();
+    document.getElementById(a).classList.add("fulsize");
+}
